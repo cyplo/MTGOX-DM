047aec7f4e3a50a5cf8c08485197f99550e3f60c
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2013-01-26 10:01:02 +0100

    cleanup

--- main.rb
+++ main.rb
@@ -18,19 +18,7 @@ puts
 
 client = MtGoxClient.new @currency, @key, @secret
 
-trend = client.trades.trend
-puts "Last trend: #{trend.to_s.upcase}, lasting for #{client.trades.trend_over} transactions"
-puts "you have #{client.btc_balance} BTC and #{client.currency_balance} #{@currency.to_s.upcase}"
-
 puts "MtGox fee: " + client.fee.to_s
-
-def get_new_selling_price trend, previous_price
-	if trend == :up then 
-		return previous_price - 0.00001
-	else 
-		return previous_price - 0.2
-	end
-end
 
 def wait_for_funds client, money_needed
 	sleep 15 until client.currency_balance >= money_needed
@@ -40,22 +28,22 @@ def buy amount, buying_price
 	puts "[BUY]  #{amount} BTC for at most  #{buying_price} #{@currency}"
 	begin
 		MtGox.buy! amount, buying_price, @currency
-	rescue
+	rescue => error
 		puts "error buying, retrying"
+		ap error
 		sleep 15
 		retry
 	end
-	begin
-		sleep 15
-	end until MtGox.buys.empty?
+	wait_for_orders
 end
 
 def sell amount, price
 	puts "[SELL] #{amount} BTC for at least #{price} #{@currency}"
 	begin
 		MtGox.sell! amount, price, @currency
-	rescue
+	rescue => error
 		puts "error selling, retrying"
+		ap error
 		sleep 15
 		retry
 	end
@@ -64,6 +52,7 @@ end
 
 def wait_for_orders
 	begin
+		sleep 30 until MtGox.buys.empty?
 		sleep 30 until MtGox.sells.empty?
 	rescue
 		puts "error waiting, retrying"
@@ -72,22 +61,26 @@ def wait_for_orders
 end
 
 loop do
-	sleep 15
+	puts "Last trend: #{client.trades.trend.to_s.upcase}, lasting for #{client.trades.trend_over} transactions"
+	puts "you have #{client.btc_balance} BTC and #{client.currency_balance} #{@currency.to_s.upcase}"
 	client.trades.write_graph
 	last_transaction_price = client.trades.sorted.first.price.to_f
+	puts "last transaction at #{last_transaction_price}"
 	if client.currency_balance > 0 then
 		buying_price = last_transaction_price
 		if client.trades.trend == :up
-			buying_price += 0.1
+			buying_price += 0.5
 		else
-			buying_price -= 0.1
+			buying_price -= 0.2
 		end
 		amount = client.currency_balance / buying_price
+		amount -= 0.001
 		buy amount, buying_price
-		selling_price = buying_price + 0.001
+		selling_price = buying_price + 0.1
 		sell client.btc_balance, selling_price
 	else
 		puts "waiting for funds..."
 		wait_for_funds 1
 	end
+	sleep 15
 end
